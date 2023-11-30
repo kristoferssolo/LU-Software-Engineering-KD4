2760644faa87f549b4fb8655dddf5eea24a15c51
Kristofers Solo <kristians.cagulis@pm.me>
2023-11-30 09:04:02 +0200

    Initial commit

---
+++ kc22015_main.py
@@ -0,0 +1,9 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from kc22015_libadd import *
+from kc22015_libmult import *
+
+a = 13
+b = 5
+print(plus(a, b))
+print(mult(a, b))
